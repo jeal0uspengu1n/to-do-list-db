ce0da25865dd10b3ab17094329e7f9d6006d069c
jeal0uspengu1n <sutej.sharma@yahoo.com>
2022-04-06 11:34:51 +0530

    Intiall commit

--- app.js
+++ app.js
@@ -1,23 +1,75 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 const date=require(__dirname + "/mod_date.js")
 const app=express();
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect('mongodb+srv://jeal0uspengu1n:788356001@cluster1.ypnv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// , {useNewUrlParser: true}
+);
 
-var newItems=["Dev","contests"];
+
+const itemsSchema = {
+  name: String
+};
+const Item= mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "kwiz"
+});
+const item2 = new Item({
+  name: "aish"
+});
+
+const defaultItems= [item1, item2];
+
+// var newItems=[];
 app.get("/",function(req,res){
   // res.send("this too bitchhhh");
-  var day=date();
-  res.render("list",{day: day, newItems: newItems});
+  Item.find({}, function(err, foundItems){
+    // console.log(foundItems);
+    if(foundItems.length === 0)
+    {
+        Item.insertMany(defaultItems, function(err){
+          if(err)
+            console.log(err);
+          else
+            console.log("NIONoiewnoif");
+        });
+        res.redirect("/");
+    }
+    else
+    {
+      var day=date();
+      res.render("list",{day: day, newItems: foundItems});
+    }
+  });
 });
 
 app.post("/",function(req,res){
   var newItem=req.body.new_list_item;
-  newItems.push(newItem);
+  // newItems.push(newItem);
   // console.log(newItem);
+  const itemX = new Item({
+    name: newItem
+  });
+  var itemXX=[itemX];
+  Item.insertMany(itemXX, function(err){
+    if(err)
+    console.log(err);
+  });
+  res.redirect("/");
+});
+
+app.post("/delete", function(req,res){
+  const delpls = req.body.del;
+  Item.findByIdAndRemove( delpls , function(err){
+    if(err)
+      console.log(err);
+  });
   res.redirect("/");
 });
 
